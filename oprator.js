5957e3529a2052f8df9d58267f5a0e333e73a213
Priyanshu Vishwakarma <vishwakarmapriyanshu007@gmail.com>
2024-11-26 20:04:40 +0530

    Add files via upload

---
+++ oprator.js
@@ -0,0 +1,33 @@
+//Airthmetic oparator
+let a = 10;
+let b = 20;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+//Unary Oparator
+let c = 10;
+let d = 2;
+console.log(c++);
+console.log(++c);
+
+
+//Assignment Oprator
+let e = 10;
+let f = 20;
+console.log(e += 1);
+console.log();
+
+//comaprison oparator
+let age = 18;
+console.log(age > 18);
+console.log(age < 18);
+console.log(age >= 18);
+console.log(age <= 18);
+console.log(age == 18);
+console.log(age != 18);
+console.log(age != 18);
+console.log(age === 18);
