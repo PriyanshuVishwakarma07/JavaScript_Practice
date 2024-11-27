fed4771411ad5f21e204b9f86c0910227c4e6b3e
Priyanshu Vishwakarma <vishwakarmapriyanshu007@gmail.com>
2024-11-27 08:55:05 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,57 @@
+// 1st example
+let classInfo = {
+    name: "Priyanshu",
+    class: "TYIT",
+    marks: 91,
+    city: "mumbai"
+};
+
+console.log(classInfo);
+console.log(classInfo.name);
+console.log(classInfo["marks"]);
+
+// 2nd Example
+
+let delhi = {
+lattitude : 28.989,
+lnhitude : "38478378.47 E"
+}
+
+console.log(delhi);
+console.log(delhi.lattitude);
+
+// Objects of objects
+let classInfoo = {
+aman : {
+    marks: 89,
+    grade: 'A'
+},
+raman : {
+    marks : 90,
+    grade : 'O'
+},
+vishal : {
+    marks : 99,
+    grade : 'O'
+}
+};
+
+
+// Array of Objects
+let classInfooo = [
+{
+    name : "priyanshu",
+    class : "TYIT",
+    marks : 89
+},
+{
+    name : "Pratibha",
+    class : "Dropper",
+    marks : "None"
+},
+{
+    name : "vishal",
+    class : "TYIT",
+    marks : 98
+}
+];
